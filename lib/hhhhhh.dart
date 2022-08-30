fbf874ed9f90ed8a6aca88cf357c473c38fa03a4
NabaaArt <Nabaa.khan@gmail.com>
2022-08-30 14:04:52 +0300

    kjhgf

---
+++ lib/hhhhhh.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class hhh extends StatefulWidget {
+  const hhh({Key? key}) : super(key: key);
+
+  @override
+  State<hhh> createState() => _hhhState();
+}
+
+class _hhhState extends State<hhh> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30,
+      width: 140,
+      color: Colors.cyan,
+    );
+  }
+}
